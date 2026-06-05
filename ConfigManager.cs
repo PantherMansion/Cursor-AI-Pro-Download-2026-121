44b4305014793351198a0ad0532bd12cf316194c
PantherMansion <rlskakgul@gmail.com>
2026-06-05 06:26:20 -0400

    Add file

---
+++ ConfigManager.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace CursorProUtility
+{
+    public class ConfigManager
+    {
+        public void ApplySettings()
+        {
+            Console.WriteLine("Applying local Pro feature flags to configuration file...");
+            // Simulated success
+            Console.WriteLine("Pro features enabled successfully.");
+        }
+    }
+}
